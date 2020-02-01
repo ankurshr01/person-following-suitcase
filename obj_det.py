caa1bf38d42ca67d4faaec96070952378339c24f
ankurshr01 <ankursharma05031999@gmail.com>
2020-02-01 22:34:47 +0530

    avg

--- obj_det.py
+++ obj_det.py
@@ -3,7 +3,6 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-
 hp = 0
 xp = 0
 yp = 0
@@ -23,31 +22,40 @@ while True:
         blue_area = max(bluecnts, key=cv2.contourArea)
         (xg,yg,wg,hg) = cv2.boundingRect(blue_area)
         cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
-        
-        if xg<xp:
-            print("right: ", str(xp-xg)+" px/frame |", end='')
-        elif xg>xp:
-            print(" left: ", str(xg-xp)+" px/frame |", end='')
-        else:
-            print(' same: ', str(xp-xg)+" px/frame |", end='')
-        xp = xg
+        sum_x=0
+        sum_y=0
+        sum_w=0
+        sum_h=0
+        for x in range(100):
+            sum_x=sum_x+xg
+            sum_y=sum_y+yg
+            sum_w=sum_w+wg
+            sum_h=sum_h+hg
+            if(x==99):
+                if sum_x<xp:
+                    print("right: ", str(xp-sum_x)+" px/frame |",end='')
+                elif sum_x>xp:
+                    print(" left: ", str(sum_x-xp)+" px/frame |",end='')
+                else:
+                    print(' same: ', str(xp-sum_x)+" px/frame |",end='')
+                xp = sum_x
+                if sum_y>yp:
+                    print("down: ", str(sum_y-yp)+" px/frame |",end='')
+                elif sum_y<yp:
+                    print(" top: ", str(yp-sum_y)+" px/frame |",end='')
+                else:
+                    print('same: ', str(sum_y-yp)+" px/frame |",end='')
+                yp = sum_y
 
-        if yg>yp:
-            print("down: ", str(yg-yp)+" px/frame |",end='')
-        elif yg<yp:
-            print(" top: ", str(yp-yg)+" px/frame |", end='')
-        else:
-            print('same: ', str(yg-yp)+" px/frame |", end='')
-        yp = yg
 
-
-        if hg<hp:
-            print('far', str(hp-hg)+" px/frame |")
-        elif hg>hp:
-            print('near', str(hg-hp)+" px/frame |")
-        else:
-            print('same', str(hg-hp)+" px/frame |")
-        hp = hg
+                if sum_h<hp:
+                    print('far', str(hp-sum_h)+" px/frame |",end='')
+                elif sum_h>hp:
+                    print('near', str(sum_h-hp)+" px/frame |",end='')
+                else:
+                    print('same', str(sum_h-hp)+" px/frame |",end='')
+                hp = sum_h
+                i=0;
         
         
 
